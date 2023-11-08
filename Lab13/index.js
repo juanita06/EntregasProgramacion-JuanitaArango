cdc673a5386076f52f4dcec2376345bd3e8a801f
juanita06 <73260991+juanita06@users.noreply.github.com>
2023-11-07 23:16:37 -0500

    Add files via upload
    Signed-off-by: juanita06 <73260991+juanita06@users.noreply.github.com>

---
+++ Lab13/index.js
@@ -0,0 +1,127 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtener referencias a elementos HTML
+    const taskForm = document.getElementById("task-form");
+    const taskInput = document.getElementById("task-input");
+    const todoContainer = document.getElementById("todo-container");
+    const doingContainer = document.getElementById("doing-container");
+    const doneContainer = document.getElementById("done-container");
+
+    // Función para crear una tarjeta de tarea con los botones
+    function createTaskCard(text, status) {
+        const card = document.createElement("div");
+        card.className = "task-card";
+        card.dataset.status = status; // Agregar el estado como atributo dataset
+
+        card.innerHTML = `
+        <div class="arriba">
+        <button class="delete-button">X</button>
+        </div>
+        <div class="cajatexto">
+        <p class="texto">${text}</p>
+        </div>
+        <div class="abajo">           
+        <button class="move-up-button"></button>
+        <button class="move-down-button"></button>
+        </div>    
+        `;                   
+    
+
+        // Agregar evento para cambiar el estado de la tarjeta
+        const moveUpButton = card.querySelector(".move-up-button");
+        moveUpButton.addEventListener("click", () => {
+            changeTaskStatus(card, "up");
+        });
+
+        const moveDownButton = card.querySelector(".move-down-button");
+        moveDownButton.addEventListener("click", () => {
+            changeTaskStatus(card, "down");
+        });
+
+        // Agregar evento para eliminar la tarjeta
+        card.querySelector(".delete-button").addEventListener("click", () => {
+            deleteTask(card);
+        });
+
+        return card;
+    }
+
+    // Función para cambiar el estado de la tarjeta
+    function changeTaskStatus(card, direction) {
+        const currentContainer = card.parentElement;
+        const newStatus = getNextStatus(card, direction);
+
+        // Mover la tarjeta al nuevo contenedor (To Do, Doing o Done)
+        const newContainer = getContainerByStatus(newStatus);
+        newContainer.appendChild(card);
+
+        // Actualizar el estado de la tarjeta
+        card.dataset.status = newStatus;
+
+        // Guardar la tarea en localStorage
+        saveTasks();
+    }
+
+    // Función para obtener el siguiente estado
+    function getNextStatus(card, direction) {
+        const statuses = ["To Do", "Doing", "Done"];
+        const currentStatus = card.dataset.status;
+        const currentIndex = statuses.indexOf(currentStatus);
+
+        if (direction === "up" && currentIndex > 0) {
+            return statuses[currentIndex - 1];
+        } else if (direction === "down" && currentIndex < 2) {
+            return statuses[currentIndex + 1];
+        }
+
+        return currentStatus;
+    }
+
+    // Función para obtener el contenedor por estado
+    function getContainerByStatus(status) {
+        switch (status) {
+            case "To Do":
+                return todoContainer;
+            case "Doing":
+                return doingContainer;
+            case "Done":
+                return doneContainer;
+        }
+    }
+
+    // Función para eliminar la tarjeta
+    function deleteTask(card) {
+        const currentContainer = card.parentElement;
+        currentContainer.removeChild(card);
+        saveTasks(tarea);
+    }
+    // Manejar el envío del formulario
+    taskForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+        const text = taskInput.value;
+        if (text) {
+            // Crea una tarjeta To Do y almacénala en localStorage
+            const taskCard = createTaskCard(text, "To Do");
+            todoContainer.appendChild(taskCard);
+            taskInput.value = "";
+           
+        }
+
+        function saveTasks(tarea) {
+
+            const tareas = JSON.parse(localStorage.getItem("tareas")) || {};
+            tareas[tarea.texto] = tarea;
+            localStorage.setItem("tareas", JSON.stringify(tareas));
+          }
+          const tareasGuardadas = JSON.parse(localStorage.getItem("tareas")) || {};
+
+          for (const tareaId in tareasGuardadas) {
+            agregarTarea(tareasGuardadas[tareaId]);
+          }
+    });
+
+});
+
+
+
+
+
